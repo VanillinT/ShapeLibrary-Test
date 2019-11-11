61b8c12fe5f99a31e7cc9ad0dae123f2402bf342
DESKTOP-505MN6E\Vanya <tominec98@gmail.com>
2019-11-11 16:41:24 +0300

    checking if triangle is right

--- ShapeLibrary/Triangle.cs
+++ ShapeLibrary/Triangle.cs
@@ -6,20 +6,29 @@ namespace ShapeLibrary
 {
     public class Triangle : IShape
     {
+        //a is presumably a hypotenuse
         double a, b, c;
+        public bool IsRight { get; private set; }
+
         public Triangle(double side1, double side2, double side3)
         {
             if (side1 <= 0 || side2 <= 0 || side3 <= 0) throw new ArgumentOutOfRangeException();
 
-            a = side1;
-            b = side2;
-            c = side3;
+            //assigning the longest side to variable 'a'
+            if (side1 >= side2 && side1 >= side3) { a = side1;  b = side2; c = side3; }
+            else if (side2 >= side1 && side2 >= side3) { a = side2; b = side1; c = side3; }
+            else { a = side3; b = side1; c = side2; }
+
+            IsRight = a * a == b * b + c * c;
         }
+
+
 
         public double GetArea()
         {
             double s = (a + b + c) / 2;
             return Math.Sqrt(s * (s - a) * (s - b) * (s - c));
         }
+
     }
 }
--- ShapeLibraryTests/Tests.cs
+++ ShapeLibraryTests/Tests.cs
@@ -9,8 +9,8 @@ namespace ShapeLibraryTests
         [Fact]
         public void TestCircleAccuracy()
         {
-            double r = 5.123, expectedVal1 = Math.PI * r * r;
-            Assert.Equal(expectedVal1, new Circle(r).GetArea());
+            double r = 5.123, expectedVal = Math.PI * r * r;
+            Assert.Equal(expectedVal, new Circle(r).GetArea());
         }
 
         [Fact]
@@ -29,19 +29,31 @@ namespace ShapeLibraryTests
             double a = 3, b = 5, c = 5.4;
             double s = (a + b + c) / 2;
 
-            double expectedVal() => Math.Sqrt(s * (s - a) * (s - b) * (s - c));
+            double expectedVal = Math.Sqrt(s * (s - a) * (s - b) * (s - c));
 
-            Assert.Equal(expectedVal(), new Triangle(a,b,c).GetArea());
+            Assert.Equal(expectedVal, new Triangle(a,b,c).GetArea());
         }
 
         [Fact]
         public void TestTriangleArgumentRange()
         {
-            double r1 = 0;
-            Assert.Throws<ArgumentOutOfRangeException>(() => new Circle(r1));
+            double a = 1, b = 2, c = 0;
+            Assert.Throws<ArgumentOutOfRangeException>(() => new Triangle(a,b,c));
 
-            double r2 = -2.324;
-            Assert.Throws<ArgumentOutOfRangeException>(() => new Circle(r2));
+            a = -1; b = -2; c = 5;
+            Assert.Throws<ArgumentOutOfRangeException>(() => new Triangle(a, b, c));
+        }
+
+        [Fact]
+        public void TestTriangleIsRight()
+        {
+            double a = 5, b = 4, c = 3;
+            Assert.True(new Triangle(a, b, c).IsRight);
+            Assert.True(new Triangle(c, b, a).IsRight);
+
+
+            a = 5; b = 10; c = 20;
+            Assert.False(new Triangle(a, b, c).IsRight);
         }
     }
 }
